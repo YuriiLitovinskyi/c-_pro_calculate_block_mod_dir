5b86417e7931b51af3d86280760538c6df46e86e
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2020-10-16 15:55:53 +0300

    v.1.0.0

---
+++ pro_calculate_block_module_direction.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	int ppkNumber, block, module, direction;
+
+	
+	cout<<"Enter Ppk number: ";
+	cin>>ppkNumber;
+	
+	cout<<"\n";
+	
+	while(cin.fail() || ppkNumber <= 0){
+		cout<<"Error. Enter an integer value greater then 0!\n";
+		cin.clear();
+		cin.ignore(256,'\n');
+		cin>>ppkNumber;
+	};
+	
+	block = ceil(ppkNumber / 64) + (ppkNumber % 64 !=0);
+	module = (ceil(ppkNumber / 8) + (ppkNumber % 8 !=0)) - ((ceil(ppkNumber / 64) + (ppkNumber % 64 !=0)) * 8 - 8);
+	direction = ppkNumber - floor(ppkNumber / 8) * 8;
+	
+	if(direction == 0){
+		direction += 8;
+	};
+	
+	cout<<"Block: "<<block<<endl;
+	cout<<"Module: "<<module<<endl;
+	cout<<"Direction: "<<direction<<endl;
+	
+	cout<<"\n";	
+	
+	system("pause");
+	return 0;
+}
